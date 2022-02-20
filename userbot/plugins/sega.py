bf3d342a874650b3cec02ae93efb076bb15ea6d5
MdNoor786 <MdNoor786@users.noreply.github.com>
2022-02-20 19:42:07 +0000

    pylint:auto fixes

--- userbot/plugins/sega.py
+++ userbot/plugins/sega.py
@@ -2,15 +2,15 @@
 Available Commands
 .sega"""
 
-from telethon import events
-from userbot.utils import admin_cmd
 import asyncio
 
+from telethon import events
+
+from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="sega"))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
-
 async def _(event):
 
     if event.fwd_from:
@@ -28,32 +28,20 @@ async def _(event):
         await event.edit(input_str)
 
         animation_chars = [
-
             "8✊️===D",
-
             "8=✊️==D",
-
             "8==✊️=D",
-
             "8===✊️D",
-
             "8==✊️=D",
-
             "8=✊️==D",
-
             "8✊️===D",
-
             "8===✊️D💦",
-
             "8==✊️=D💦💦",
-
-            "8=✊️==D💦💦💦"
-
+            "8=✊️==D💦💦💦",
         ]
 
         for i in animation_ttl:
-        
-            await asyncio.sleep(animation_interval)
-        
-            await event.edit(animation_chars[i % 8])
 
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 8])
